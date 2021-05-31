529e33a523b07cce3af25fd42743f89956b8ade6
mithunpaul08 <mithunpaul08@gmail.com>
2021-05-30 21:01:40 -0700

    adding scaling

--- predictables/CGAP/utils.py
+++ predictables/CGAP/utils.py
@@ -1,9 +1,16 @@
 import math
+import pandas as pd
 
-def checknan(val):
-    if isinstance(val,str):
-        return val
-    if math.isnan(val):
-        return -1
-    else:
-        return val
+#given a pandas series return its values as z-scaled.
+def get_z_scaled(data):
+    assert type(data) is pd.Series
+    data_scaled=data.copy()
+    data_scaled = (data_scaled - (data_scaled.mean())) / (data_scaled.std())
+    return data_scaled
+
+def scale_min_max(data):
+    assert type(data) is pd.Series
+    df_min_max_scaled = data.copy()
+    df_min_max_scaled = (df_min_max_scaled - df_min_max_scaled.min()) / (
+                df_min_max_scaled.max() - df_min_max_scaled.min())
+    return df_min_max_scaled
