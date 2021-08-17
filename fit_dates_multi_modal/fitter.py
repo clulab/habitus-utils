a640e99129e2968dd80eeb76089e72396adcbb78
mithunpaul08 <mithunpaul08@gmail.com>
2021-08-17 00:32:51 -0700

    able to print individual dates as x axis labels

--- fit_dates_multi_modal/fitter.py
+++ fit_dates_multi_modal/fitter.py
@@ -11,7 +11,7 @@ _DATE_RANGE = ('2000-05-12', '2020-05-12')
 _DATE_FORMAT = '%Y-%m-%d'
 _EMPIRICAL_SCALE_RATIO = 0.15
 _DISTRIBUTION_SIZE = 1000
-
+_SCALE_RATIO_FOR_DRAWING=1e8
 
 time_range = tuple(time.mktime(time.strptime(d, _DATE_FORMAT))
                        for d in _DATE_RANGE)
@@ -21,19 +21,47 @@ distribution = numpy.random.normal(
     scale=(time_range[1] - time_range[0]) * _EMPIRICAL_SCALE_RATIO,
     size=_DISTRIBUTION_SIZE
 )
+
+
 #s = np.array(list(time.strftime(_DATE_FORMAT, time.localtime(t))for t in numpy.sort(distribution)))
 
-distribution=np.true_divide(distribution, 1e8)
-sigma=0.1
+distribution=np.true_divide(distribution, _SCALE_RATIO_FOR_DRAWING)
+sigma=3
 mu=1193084540.5079513
-#s=random.normal(mu,sigma, size=10000)
 
 
 
 fig, axes = plt.subplots(sharex='all',sharey='all')
-count, bins, ignored = axes.hist(distribution, 30, density=True)
+
+
+
+count, bins, ignored = axes.hist(distribution, bins=30, density=True)
+
+def reverse_bins(bins):
+    dates=[]
+    for t in numpy.sort(bins):
+        indiv=reverse_dates(t)
+        dates.append(indiv)
+    return dates
+
+
+def reverse_dates(date,pos=None):
+    indiv=time.strftime(_DATE_FORMAT, time.localtime(date*_SCALE_RATIO_FOR_DRAWING))
+    return indiv
+
+
+
+
+print(reverse_bins(bins))
+
+
+
 axes.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *np.exp( - (bins - mu)**2 / (2 * sigma**2) ),linewidth=2, color='r')
 
+labels = axes.get_xticklabels()
+axes.xaxis.set_major_formatter(reverse_dates)
+plt.setp(labels, rotation=45, horizontalalignment='right')
+#axes.plot(bins,distribution,'bx')
 all_bdw=np.linspace(0.1,1,10)
 #for bndw in [0.1,0.2,0.9]:
 for bndw in all_bdw:
