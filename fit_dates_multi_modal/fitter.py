48f0bfb6a146bee73aac587c47511e764487deb1
mithunpaul08 <mithunpaul08@gmail.com>
2021-08-16 22:36:03 -0700

    adding acknowledgements of date function distribution

--- fit_dates_multi_modal/fitter.py
+++ fit_dates_multi_modal/fitter.py
@@ -1,23 +1,47 @@
+from numpy import random
 from sklearn.neighbors import KernelDensity
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('Solarize_Light2')
-from scipy.stats.distributions import norm
+#datefn from:https://stackoverflow.com/questions/39260616/generate-a-normal-distribution-of-dates-within-a-range
+import time
+import numpy
 
-y_axis = np.concatenate([norm(-1, 1.).rvs(400),norm(1, 0.3).rvs(100)])
+_DATE_RANGE = ('2000-05-12', '2020-05-12')
+_DATE_FORMAT = '%Y-%m-%d'
+_EMPIRICAL_SCALE_RATIO = 0.15
+_DISTRIBUTION_SIZE = 1000
 
-#y_axis=np.array([2,4,3,7.5,4.5,7.8,2,4,3,7.5])
-# The grid we'll use for plotting
-x_grid = np.linspace(-2, 1, 500)
 
-fig, axes = plt.subplots(sharex='all',sharey='all',)
-#axes.set(xlim=(-10,10),ylim=(-10,10))
-#x_axis=np.array(list(data.values()))
-#y_axis=np.array(list(data.keys()))
-axes.plot(x_grid,y_axis,'rx')
-all_bdw=[np.linspace(0,1,100)]
+time_range = tuple(time.mktime(time.strptime(d, _DATE_FORMAT))
+                       for d in _DATE_RANGE)
+
+distribution = numpy.random.normal(
+    loc=(time_range[0] + time_range[1]) * 0.5,
+    scale=(time_range[1] - time_range[0]) * _EMPIRICAL_SCALE_RATIO,
+    size=_DISTRIBUTION_SIZE
+)
+#s = np.array(list(time.strftime(_DATE_FORMAT, time.localtime(t))for t in numpy.sort(distribution)))
+
+distribution=np.true_divide(distribution, 1e8)
+sigma=0.1
+mu=1193084540.5079513
+#s=random.normal(mu,sigma, size=10000)
+
+
+
+fig, axes = plt.subplots(sharex='all',sharey='all')
+count, bins, ignored = axes.hist(distribution, 30, density=True)
+axes.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *np.exp( - (bins - mu)**2 / (2 * sigma**2) ),linewidth=2, color='r')
+
+all_bdw=np.linspace(0.1,1,10)
+#for bndw in [0.1,0.2,0.9]:
 for bndw in all_bdw:
-    kde=KernelDensity(kernel='gaussian',bandwidth=bndw).fit(X=y_axis.reshape(-1,1))
-    log_density=kde.score_samples(x_grid.reshape(-1,1))
-    axes.plot(x_grid,(log_density),'b--')
+    kde=KernelDensity(kernel='gaussian',bandwidth=bndw).fit(X=distribution.reshape(-1, 1))
+    log_density=kde.score_samples(bins.reshape(-1,1))
+    axes.plot(bins,np.exp(log_density),'b--')
 plt.show()
+# figure,axes= plt.subplots()
+# axes.set(xlim=[3,-3],ylim=[3,-3])
+# axes.plot(y,'r--')
+# plt.show()
