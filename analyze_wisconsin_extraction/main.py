e2c7cf88bb7efbc06b543dfceb93c8abecf4bf06
Mithun Paul <mithunpaul@lane.cs.arizona.edu>
2021-11-28 17:48:07 -0700

    reads each data point. next find what all to calculate

--- analyze_wisconsin_extraction/main.py
+++ analyze_wisconsin_extraction/main.py
@@ -1,12 +1,22 @@
 import os
+import sys
+import json
 
-data_folder="data_wisconsin_parsing"
+data_folder="/Users/mithunpaul/research/habitus/data_wisconsin_parsing"
 def read_files():
-    full_path=os.path.join(os.getcwd(),data_folder)
+    full_path=data_folder
     for root, subdir, files in os.walk(full_path):
-        #print(f"{root},{subdir},{files}")
-        for tsv, json in files:
-            print(tsv)
-            
+        #print(f"files={files}")
+        for file in files:
+            if ("json") in file:
+
+                filepath=os.path.join(root,file)
+                print(filepath)
+                with open(filepath) as fp:
+                    data= (json.load(fp))
+                    for datapoint in data:
+                        print(datapoint['mostFreqLoc'])
+                        sys.exit()
+
 if __name__ == '__main__':
     read_files()
