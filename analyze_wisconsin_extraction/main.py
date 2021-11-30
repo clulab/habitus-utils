b4faece067eb1b24b421f709343744ddd8a144a9
Mithun Paul <mithunpaul@lane.cs.arizona.edu>
2021-11-29 17:36:45 -0700

    still trying to print a saner sentence which has all 3, senegal, crop year, in it

--- analyze_wisconsin_extraction/main.py
+++ analyze_wisconsin_extraction/main.py
@@ -11,13 +11,11 @@ value_counter=collections.Counter()
 
 
 # takes a json datapoint and increases Counter
-def update_sentence_level_counters(datapoint):
-    with open("stdout.txt","w") as stdout:
+def update_sentence_level_counters(datapoint,stdout):
 
         has_senegal_anywhere_in_the_sentence = False
         has_senegal_and_year_in_the_same_sentence = False
         has_senegal_and_crop_in_the_same_sentence = False
-
         value_counter.update(['total_sentences'])
 
         #parse this line only if (its digest) it was not seen before
@@ -82,29 +80,28 @@ def update_sentence_level_counters(datapoint):
                 value_counter.update(['has_senegal_year_and_crop_same_sentence'])
                 stdout.write(f"sample sentence for  has_senegal_year_and_crop_same_sentence:{datapoint}")
                 print(f"sample sentence for  has_senegal_year_and_crop_same_sentence:{datapoint}")
-                exit()
+
 
             #- How many lines have all three. YEAR, CROP, LOC in same sentence
             if (not datapoint['mostFreqDate0Sent'] == "N/A") and (not datapoint['mostFreqCrop0Sent']== "N/A") and (not datapoint['mostFreqLoc0Sent']== "N/A"):
                 value_counter.update(['has_year_crop_loc_all3_samesent'])
 
-            stdout.close()
             return value_counter
 
 def read_files():
     full_path=os.path.join(os.getcwd(),data_folder)
-    for root, subdir, files in os.walk(full_path):
-        for index_files,file in enumerate(files):
-            if ("json") in file:
-                filepath=os.path.join(root,file)
-                print(filepath)
-                with open(filepath) as fp:
-                    data= (json.load(fp))
-                    for index_data,datapoint in enumerate(data):
-                        #datapoint= json.loads(datapoint.decode("utf-8", "ignore"))
-                        update_sentence_level_counters(datapoint)
-                        with open("analysis.json", "w") as out:
-                            json.dump(value_counter, out,indent=4)
+    with open("stdout.txt", "w") as stdout:
+        for root, subdir, files in os.walk(full_path):
+            for index_files,file in enumerate(files):
+                if ("json") in file:
+                    input_filepath=os.path.join(root,file)
+                    print(input_filepath)
+                    with open(input_filepath) as fp:
+                        data= (json.load(fp))
+                        for index_data,datapoint in enumerate(data):
+                            update_sentence_level_counters(datapoint,stdout)
+                            with open("analysis.json", "w") as out:
+                                json.dump(value_counter, out,indent=4)
 
 
 
