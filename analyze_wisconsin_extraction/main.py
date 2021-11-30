ed2d228f1946f0af0bd4ebb2dd7104b4ec79af28
Mithun Paul <mithunpaul@lane.cs.arizona.edu>
2021-11-29 18:15:19 -0700

    will write every line to a file called all+lines

--- analyze_wisconsin_extraction/main.py
+++ analyze_wisconsin_extraction/main.py
@@ -12,89 +12,92 @@ value_counter=collections.Counter()
 
 # takes a json datapoint and increases Counter
 def update_sentence_level_counters(datapoint):
-    with open("stdout.txt", "a") as stdout:
+    with open("stdout.txt", "a") as stdout , open("all_lines.txt", "a") as allines:
         has_senegal_anywhere_in_the_sentence = False
         has_senegal_and_year_in_the_same_sentence = False
         has_senegal_and_crop_in_the_same_sentence = False
         value_counter.update(['total_sentences'])
-
+        allines.write(f"sample sentence for  has_senegal_anywhere_in_the_sentence:{datapoint}\n")
 
         if(datapoint['sentenceText'] not in set_all_lines):
-            set_all_lines.add(datapoint['sentenceText'])
-            value_counter['total_unique_sentences'] = len(set_all_lines)
+                set_all_lines.add(datapoint['sentenceText'])
+                value_counter['total_unique_sentences'] = len(set_all_lines)
 
-            # How many rows have Senegal anywhere.
-            for k, v in datapoint.items():
-                if ("senegal" in v.lower()):
-                    value_counter.update(['has_senegal_anywhere_in_the_sentence'])
-                    has_senegal_anywhere_in_the_sentence=True
-                    stdout.write(f"sample sentence for  has_senegal_anywhere_in_the_sentence:{datapoint}\n")
-                    break
+                # How many rows have Senegal anywhere.
+                for k, v in datapoint.items():
+                    if ("senegal" in v.lower()):
+                        value_counter.update(['has_senegal_anywhere_in_the_sentence'])
+                        has_senegal_anywhere_in_the_sentence=True
+                        stdout.write(f"sample sentence for  has_senegal_anywhere_in_the_sentence:{datapoint}\n")
+                        break
 
-            # How many rows have Senegal as mostFreqLoc.
-            if datapoint['mostFreqLoc'].lower() == "senegal":
-                value_counter.update(['has_senegal_as_mostFreqLoc'])
-                stdout.write(f"sample sentence for  has_senegal_as_mostFreqLoc:{datapoint}\n")
-
-
-
-            # How many rows have some context anywhere.
-            for k, v in datapoint.items():
-                if ("mostFreq" in k) and (not v == "N/A"):
-                    value_counter.update(['has_some_context'])
-                    stdout.write(f"sample sentence for  has_some_context:{datapoint}\n")
-                    break
-
-            # how many sentences has a location in the same sentence
-            if not datapoint['mostFreqLoc0Sent'] == "N/A":
-                value_counter.update(['has_loc_same_sentence'])
-                stdout.write(f"sample sentence for  has_loc_same_sentence:{datapoint}\n")
-
-            # - How many lines have YEAR.
-            if not datapoint['mostFreqDate0Sent'] == "N/A":
-                # How many lines have senegal somewhere and also has a YEAR mentioned
-                if has_senegal_anywhere_in_the_sentence==True:
-                    value_counter.update(['has_senegal_and_year_same_sentence'])
-                    has_senegal_and_year_in_the_same_sentence = True
-                    stdout.write(f"sample sentence for  has_senegal_and_year_same_sentence:{datapoint}\n")
-                value_counter.update(['has_year_same_sentence'])
-                stdout.write(f"sample sentence for  has_year_same_sentence:{datapoint}\n")
+                # How many rows have Senegal as mostFreqLoc.
+                if datapoint['mostFreqLoc'].lower() == "senegal":
+                    value_counter.update(['has_senegal_as_mostFreqLoc'])
+                    stdout.write(f"sample sentence for  has_senegal_as_mostFreqLoc:{datapoint}\n")
 
 
-                # How many lines have senegal as mostFreqLoc0Sent and also has a value for mostFreqDate0Sent
+
+                # How many rows have some context anywhere.
+                for k, v in datapoint.items():
+                    if ("mostFreq" in k) and (not v == "N/A"):
+                        value_counter.update(['has_some_context'])
+                        stdout.write(f"sample sentence for  has_some_context:{datapoint}\n")
+                        break
+
+                # how many sentences has a location in the same sentence
+                if not datapoint['mostFreqLoc0Sent'] == "N/A":
+                    value_counter.update(['has_loc_same_sentence'])
+                    stdout.write(f"sample sentence for  has_loc_same_sentence:{datapoint}\n")
+
+                # - How many lines have YEAR.
                 if not datapoint['mostFreqDate0Sent'] == "N/A":
-                    if not datapoint['mostFreqLoc'] == "N/A" and datapoint['mostFreqLoc'].lower() == "senegal":
-                        value_counter.update(['has_senegal_as_mostFreqLoc0Sent_and_a_mostFreqDate0Sent'])
-                        stdout.write(f"sample sentence for  has_senegal_as_mostFreqLoc0Sent_and_a_mostFreqDate0Sent:{datapoint}\n")
-
-            # how many sentences has a crop in the same sentence
-            if not datapoint['mostFreqCrop0Sent'] == "N/A":
-                # How many lines have senegal somewhere and also has a CROP mentioned
-                if has_senegal_anywhere_in_the_sentence == True:
-                    value_counter.update(['has_senegal_and_crop_same_sentence'])
-                    has_senegal_and_crop_in_the_same_sentence = True
-                    stdout.write(f"sample sentence for  has_senegal_and_crop_same_sentence:{datapoint}\n")
-                value_counter.update(['has_crop_same_sentence'])
-                stdout.write(f"sample sentence for  has_crop_same_sentence:{datapoint}\n")
-
-            if(has_senegal_and_crop_in_the_same_sentence ==True and has_senegal_and_year_in_the_same_sentence==True):
-                value_counter.update(['has_senegal_year_and_crop_same_sentence'])
-                stdout.write(f"sample sentence for  has_senegal_year_and_crop_same_sentence:{datapoint}\n")
+                    # How many lines have senegal somewhere and also has a YEAR mentioned
+                    if has_senegal_anywhere_in_the_sentence==True:
+                        value_counter.update(['has_senegal_and_year_same_sentence'])
+                        has_senegal_and_year_in_the_same_sentence = True
+                        stdout.write(f"sample sentence for  has_senegal_and_year_same_sentence:{datapoint}\n")
+                    value_counter.update(['has_year_same_sentence'])
+                    stdout.write(f"sample sentence for  has_year_same_sentence:{datapoint}\n")
 
 
+                    # How many lines have senegal as mostFreqLoc0Sent and also has a value for mostFreqDate0Sent
+                    if not datapoint['mostFreqDate0Sent'] == "N/A":
+                        if not datapoint['mostFreqLoc'] == "N/A" and datapoint['mostFreqLoc'].lower() == "senegal":
+                            value_counter.update(['has_senegal_as_mostFreqLoc0Sent_and_a_mostFreqDate0Sent'])
+                            stdout.write(f"sample sentence for  has_senegal_as_mostFreqLoc0Sent_and_a_mostFreqDate0Sent:{datapoint}\n")
 
-            #- How many lines have all three. YEAR, CROP, LOC in same sentence
-            if (not datapoint['mostFreqDate0Sent'] == "N/A") and (not datapoint['mostFreqCrop0Sent']== "N/A") and (not datapoint['mostFreqLoc0Sent']== "N/A"):
-                value_counter.update(['has_year_crop_loc_all3_samesent'])
-                stdout.write(f"sample sentence for  has_year_crop_loc_all3_samesent:{datapoint}\n")
+                # how many sentences has a crop in the same sentence
+                if not datapoint['mostFreqCrop0Sent'] == "N/A":
+                    # How many lines have senegal somewhere and also has a CROP mentioned
+                    if has_senegal_anywhere_in_the_sentence == True:
+                        value_counter.update(['has_senegal_and_crop_same_sentence'])
+                        has_senegal_and_crop_in_the_same_sentence = True
+                        stdout.write(f"sample sentence for  has_senegal_and_crop_same_sentence:{datapoint}\n")
+                    value_counter.update(['has_crop_same_sentence'])
+                    stdout.write(f"sample sentence for  has_crop_same_sentence:{datapoint}\n")
+
+                if(has_senegal_and_crop_in_the_same_sentence ==True and has_senegal_and_year_in_the_same_sentence==True):
+                    value_counter.update(['has_senegal_year_and_crop_same_sentence'])
+                    stdout.write(f"sample sentence for  has_senegal_year_and_crop_same_sentence:{datapoint}\n")
 
 
-            return value_counter
+
+                #- How many lines have all three. YEAR, CROP, LOC in same sentence
+                if (not datapoint['mostFreqDate0Sent'] == "N/A") and (not datapoint['mostFreqCrop0Sent']== "N/A") and (not datapoint['mostFreqLoc0Sent']== "N/A"):
+                    value_counter.update(['has_year_crop_loc_all3_samesent'])
+                    stdout.write(f"sample sentence for  has_year_crop_loc_all3_samesent:{datapoint}\n")
+
+
+                return value_counter
+def initialize_file(filename):
+    fp = open(filename, "w")
+    fp.close()
 
 def read_files():
     full_path=os.path.join(os.getcwd(),data_folder)
-    fp=open("stdout.txt", "w")
-    fp.close()
+    initialize_file("stdout.txt")
+    initialize_file("all_lines.txt")
     for root, subdir, files in os.walk(full_path):
         for index_files,file in enumerate(files):
             if ("json") in file:
